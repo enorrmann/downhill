aa72bc80468e05f0410e7fd75441c91367d49eaf
Emilio <enorrmann@gmail.com>
2020-05-17 00:43:33 +0200

    inter

--- src/tiles.c
+++ src/tiles.c
@@ -6,7 +6,7 @@
 #define TILE_DIAG_2 7
 #define TILE_DIAG_3 8
 
-#define NUM_TILES 15
+#define NUM_TILES 5
 #define ULTIMO_TILE NUM_TILES - 1
 #define DELAY 3
 #define FACTOR 2
@@ -36,10 +36,10 @@ const u32 tileDiag[8] =
 
 const u32 tileDiag2[8] =
     {
-        0x00000000,
         0xFF000000,
         0xFFFF0000,
         0xFFFFFF00,
+        0xFFFFFFFF,
         0xFFFFFFFF,
         0xFFFFFFFF,
         0xFFFFFFFF,
@@ -51,10 +51,10 @@ const u32 tileDiag3[8] =
         0x00000000,
         0x00000000,
         0x00000000,
-        0xF0000000,
         0xFF000000,
         0xFFFF0000,
-        0xFFFFFF00};
+        0xFFFFFF00,
+        0xFFFFFFFF};
 
 u8 delay_count = 0;
 s8 direccion = D_NULL;
@@ -66,6 +66,7 @@ u8 rotate = 0;
 u8 tiles_y[NUM_TILES];
 s8 tiles_direction[NUM_TILES];
 s8 tiles_shape[4];
+s8 tiles_next_shape[4];
 
 void TILES_init()
 {
@@ -79,6 +80,10 @@ void TILES_init()
     tiles_shape[2] = TILE_DIAG;  // down
     tiles_shape[3] = TILE_WHITE; // right
     tiles_shape[4] = TILE_WHITE; // SCROLL_UP
+
+    tiles_next_shape[0] = TILE_DIAG_3; // up
+    tiles_next_shape[1] = TILE_WHITE;  // null
+    tiles_next_shape[2] = TILE_DIAG_3; // down
 
     for (int i = 0; i < NUM_TILES; i++)
     {
@@ -178,11 +183,11 @@ static void avanzar()
         // "borro" el tile de arriba si va para abajo, sino, blanco abajo
         if (tiles_y[i] < tiles_y[i + 1])
         {
-            VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL0, 1, 0, 0, TILE_BLACK), x, tiles_y[i]);
+          //  VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL0, 1, 0, 0, TILE_BLACK), x, tiles_y[i]);
         }
         else
         {
-            VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL0, 1, 0, 0, TILE_WHITE), x, tiles_y[i]);
+           // VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL0, 1, 0, 0, TILE_WHITE), x, tiles_y[i]);
         }
         tiles_y[i] = tiles_y[i + 1];                 // rotata posiciones
         tiles_direction[i] = tiles_direction[i + 1]; // rotates direccion en ese momento
@@ -191,7 +196,10 @@ static void avanzar()
         // ESTE ES EL QUE DIBUJA EL KEY TILE, O SEA EL TILE QUE MANDA
         // esto se ejecuta para todas los tiles de arriba
         rotate = tiles_direction[i] == D_UP;
+        //rotate = 0 ;
         VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL0, 1, 0, rotate, tiles_shape[tiles_direction[i] + 1]), x, tiles_y[i]);
+        //int inter_x = x + 1 * direccion;
+        VDP_setTileMapXY(PLAN_A, TILE_ATTR_FULL(PAL0, 1, 0, rotate, tiles_next_shape[tiles_direction[i] + 1]), x+1, tiles_y[i] + 1*direccion);
         //  }
         if (debug)
             TILES_DEBUG();
@@ -242,9 +250,9 @@ static void TILES_DEBUG()
     sprintf(debug_string, "direccion %4d", direccion);
     VDP_drawText(debug_string, 20, 10);
 
-    /*    for (int i = 0; i <= ULTIMO_TILE; i++)
+    for (int i = 0; i <= ULTIMO_TILE; i++)
     {
         sprintf(debug_string, "y,d,s %2d,%2d,%2d,%2d", i, tiles_y[i], tiles_direction[i], tiles_shape[tiles_direction[i] + 1]);
         VDP_drawText(debug_string, 20, 11 + i);
-    }*/
+    }
 }
